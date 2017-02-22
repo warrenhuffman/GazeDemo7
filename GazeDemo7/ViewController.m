e61ee101dc17a8db9b8306ca25712a0bb97dbaab
Warren Huffman <warrenhuffman@warrens-mbp>
2017-02-22 15:08:37 -0600

    Initial Commit

---
+++ GazeDemo7/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GazeDemo7
+//
+//  Created by Warren Huffman on 2/22/17.
+//  Copyright © 2017 Locus Tracking. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GazeDemo7Tests/GazeDemo7Tests.m
@@ -0,0 +1,39 @@
+//
+//  GazeDemo7Tests.m
+//  GazeDemo7Tests
+//
+//  Created by Warren Huffman on 2/22/17.
+//  Copyright © 2017 Locus Tracking. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GazeDemo7Tests : XCTestCase
+
+@end
+
+@implementation GazeDemo7Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
